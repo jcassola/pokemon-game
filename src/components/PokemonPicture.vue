e830b21ac9d77f191be03e568d510f26647caecc
Elodin <jcassola96@gmail.com>
2022-06-10 16:53:55 -0400

    upload project

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="pokemon-container">
-    <img
+    <img v-if="!showPokemon"
       :src="imgSrc"
       alt="pokemon"
       class="hidden-pokemon" />
-    <img
-    v-if="showPokemon"
+    <img v-else
     :src="imgSrc"
     alt="pokemon"
     class="fade-in">
