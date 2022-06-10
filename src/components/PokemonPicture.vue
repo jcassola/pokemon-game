958f49d65482d40513da895a5bb04cc50760bf59
Elodin <jcassola96@gmail.com>
2022-06-10 10:46:39 -0400

    define project structure

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>Pokemon options
+  </h3>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Quien es este pokemon</h1>
+
+    <pokemon-picture />
+
+    <pokemon-options />
+
+  </div>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+  components: { PokemonPicture, PokemonOptions },
+
+}
+</script>
+
+<style>
+
+</style>
